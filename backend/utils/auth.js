5468ee8e702a0b2ec12970020e8e91ea28fb26f3
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-01-03 22:41:12 -0500

    loginRoute

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -9,23 +9,23 @@ const { secret, expiresIn } = jwtConfig;
 const setTokenCookie = (res, user) => {
     // Create the token.
     const token = jwt.sign(
-        { data: user.toSafeObject() },
-        secret,
-        { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+      { data: user.toSafeObject() },
+      secret,
+      { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
     );
 
     const isProduction = process.env.NODE_ENV === "production";
 
     // Set the token cookie
     res.cookie('token', token, {
-        maxAge: expiresIn * 1000, // maxAge in milliseconds
-        httpOnly: true,
-        secure: isProduction,
-        sameSite: isProduction && "Lax"
+      maxAge: expiresIn * 1000, // maxAge in milliseconds
+      httpOnly: true,
+      secure: isProduction,
+      sameSite: isProduction && "Lax"
     });
 
     return token;
-};
+  };
 
 
 const restoreUser = (req, res, next) => {
