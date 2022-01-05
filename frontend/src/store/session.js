495f1c43c60e2b6f71e5cfec6bf2da757d4f0266
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-01-04 23:09:28 -0500

    SignupComponent

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,6 +16,7 @@ const removeUser = () => {
   };
 };
 
+
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch('/api/session', {
@@ -30,8 +31,25 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+
 export const restoreUser = () => async dispatch => {
     const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+
+  export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
     const data = await response.json();
     dispatch(setUser(data.user));
     return response;
