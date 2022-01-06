1c4cb79448c8dcf580318910ca8b1e9817782c71
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-01-05 23:56:31 -0500

    signupLoginDropdownDoneNeedsToBeDry

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -3,12 +3,14 @@ import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 import LoginFormModal from "../LoginFormModal";
 import LoginForm from "../LoginFormModal/LoginForm";
+import SignupForm from "../SignupFormModal/SignupForm";
 import { Modal } from '../../context/Modal';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
-  const [showModal, setShowModal] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [showSignupModal, setShowSignupModal] = useState(false);
 
 
   const openMenu = () => {
@@ -57,14 +59,20 @@ function ProfileButton({ user }) {
           </button>
           {showMenu && (
             <ul className="profile-dropdown">
-              <li><button onClick={() => setShowModal(true)}>Log In</button></li>
+              <li><button onClick={() => setShowLoginModal(true)}>Log In</button></li>
+              <li><button onClick={() => setShowSignupModal(true)}>Sign Up</button></li>
             </ul>
 
           )}
 
-          {showModal && (
-            <Modal onClose={() => setShowModal(false)}>
+          {showLoginModal && (
+            <Modal onClose={() => setShowLoginModal(false)}>
               <LoginForm />
+            </Modal>
+          )}
+          {showSignupModal && (
+            <Modal onClose={() => setShowSignupModal(false)}>
+              <SignupForm />
             </Modal>
           )}
         </>
