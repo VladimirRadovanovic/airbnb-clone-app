b8f935efe63f1bbd61213f690761b67bc083e322
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-01-08 19:47:19 -0500

    addedReturnForHandelSubmitCreateSpot

--- frontend/src/components/CreateSpotForm/CreateSpotForm.js
+++ frontend/src/components/CreateSpotForm/CreateSpotForm.js
@@ -9,19 +9,20 @@ import { items } from "../DropdownCombobox/utils";
 
 import './CreateSpot.css';
 
-function CreateSpotForm({ setShowModal }) {
+
+function CreateSpotForm({ setShowModal, spot }) {
     const dispatch = useDispatch();
     const sessionUser = useSelector((state) => state.session.user);
-    const [title, setTitle] = useState("");
-    const [address, setAddress] = useState("");
-    const [city, setCity] = useState("");
-    const [state, setState] = useState('');
-    const [zipCode, setZipCode] = useState("");
-    const [country, setCountry] = useState("");
-    const [price, setPrice] = useState("");
-    const [bedrooms, setBedrooms] = useState('');
-    const [bathrooms, setBathrooms] = useState('');
-    const [description, setDescription] = useState("");
+    const [title, setTitle] = useState(spot?.title || "");
+    const [address, setAddress] = useState(spot?.address || "");
+    const [city, setCity] = useState(spot?.city || "");
+    const [state, setState] = useState(spot?.state || '');
+    const [zipCode, setZipCode] = useState(spot?.zipCode || "");
+    const [country, setCountry] = useState(spot?.country || "");
+    const [price, setPrice] = useState(spot?.price || "");
+    const [bedrooms, setBedrooms] = useState(spot?.bedrooms || '');
+    const [bathrooms, setBathrooms] = useState(spot?.bathrooms || '');
+    const [description, setDescription] = useState(spot?.description || "");
     const [errors, setErrors] = useState([]);
 
     console.log(state, 'price')
@@ -33,6 +34,43 @@ function CreateSpotForm({ setShowModal }) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        setErrors([])
+
+        const spot = {
+            title,
+            address,
+            city,
+            state,
+            zipCode,
+            country,
+            price,
+            bedrooms,
+            bathrooms,
+            description
+        }
+
+        const reset = () => {
+            setTitle('')
+            setAddress('')
+            setCity('')
+            setState('')
+            setZipCode('')
+            setCountry('')
+            setPrice('')
+            setBedrooms('')
+            setBathrooms('')
+            setDescription('')
+        }
+        console.log(spot)
+        return dispatch(createSpot(spot)).then(() => reset()).then(() => setShowModal(false)).catch(
+            async(res) => {
+                const data = await res.json()
+                if (data && data.errors) setErrors(data.errors)
+
+            }
+        )
+
         // if (password === confirmPassword) {
         //     setErrors([]);
         //     return dispatch(sessionActions.signup({ email, username, password }))
