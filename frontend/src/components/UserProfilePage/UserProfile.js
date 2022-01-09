0801e2dc7f77e17374d613ff3ba35f285bfbddd0
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-01-09 13:44:07 -0500

    addedNavLinksListingsBookingsReviwes

--- frontend/src/components/UserProfilePage/UserProfile.js
+++ frontend/src/components/UserProfilePage/UserProfile.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import profileImg from '../../images/t.jpeg'
+import { NavLink, Route, Switch } from "react-router-dom";
+
 
 import UserListings from "./UserListings/UserListings";
 import './UserProfile.css'
@@ -10,7 +12,7 @@ function UserProfile({ user }) {
     const dispatch = useDispatch()
 
     return (
-        <>
+        <main className="user-profile-container">
             <div className="user-info-container">
                 <div className="user-image-container">
                     <img className="user-image" src={profileImg} alt='user profile image' />
@@ -20,8 +22,21 @@ function UserProfile({ user }) {
                     <h4>Email: {user.email}</h4>
                 </div>
             </div>
-            <UserListings user={user}/>
-        </>
+            <NavLink exact to='/api/user/profile'>Listings</NavLink>
+            <NavLink to='/api/user/profile/bookings'>Bookings</NavLink>
+            <NavLink to='/api/user/profile/reviews'>Reviews</NavLink>
+            <Switch>
+                <Route exact path='/api/user/profile'>
+                    <UserListings user={user} />
+                </Route>
+                <Route path='/api/user/profile/bookings'>
+                    <h1>dasdasd</h1>
+                </Route>
+                <Route path='/api/user/profile/reviews'>
+                    <h1>aaaaaaaaa</h1>
+                </Route>
+            </Switch>
+        </main>
     )
 }
 
