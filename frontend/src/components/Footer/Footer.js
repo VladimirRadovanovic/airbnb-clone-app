228566990c2317f8a2046597077cec2b6e605b17
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-01-13 10:56:45 -0500

    footerStylingDone

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -10,7 +10,45 @@ function Footer() {
         <footer className="footer">
             <div className='footer-main-container'>
                 <div className="top-container">
-
+                    <div>
+                        <ul>
+                            <li className='first-li'>Support</li>
+                            <li>Help Center</li>
+                            <li>Cancellation options</li>
+                            <li>Safety information</li>
+                            <li>Report a concern</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li className='first-li'>Community</li>
+                            <li>EarthBnB.org: disaster relief housing</li>
+                            <li>Support</li>
+                            <li>Celebrating diversity and belonging</li>
+                            <li>Combating discrimination</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li className='first-li'>Hosting</li>
+                            <li>Try hosting</li>
+                            <li>EarthCover: protection for Hosts</li>
+                            <li>Explore hosting resources</li>
+                            <li>Visit our community forum</li>
+                            <li>How to host responsibly</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li className='first-li'>About</li>
+                            <li>Newsroom</li>
+                            <li>Learn about new features</li>
+                            <li>Letter from our founders</li>
+                            <li>Careers</li>
+                            <li>Investors</li>
+                            <li>EarthBnB Luxe</li>
+                        </ul>
+                    </div>
                 </div>
                 <div className='bottom-container' >
                     <div className='copyright-container'>
@@ -21,8 +59,8 @@ function Footer() {
                         <span>Privacy | Terms | Sitemap</span>
                     </div>
                     <div className="outside-links-container">
-                        <Link to={{ pathname: "https://github.com/VladimirRadovanovic" }}  target="_blank"><img className='outside-link-image' src={github} alt='github link'/></Link>
-                        <Link to={{ pathname: "https://www.linkedin.com/in/vladimir-radovanovic-476311224/" }} target="_blank"><img className='outside-link-image' src={linkedin} alt='linkedin link'/></Link>
+                        <Link to={{ pathname: "https://github.com/VladimirRadovanovic" }} target="_blank"><img className='outside-link-image' src={github} alt='github link' /></Link>
+                        <Link to={{ pathname: "https://www.linkedin.com/in/vladimir-radovanovic-476311224/" }} target="_blank"><img className='outside-link-image' src={linkedin} alt='linkedin link' /></Link>
                     </div>
                 </div>
             </div>
