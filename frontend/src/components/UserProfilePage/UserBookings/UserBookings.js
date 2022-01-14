fc8dce618674c3fa17bf3c6a4b30771bd764ff89
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-01-14 10:06:02 -0500

    positioned the img in bookings

--- frontend/src/components/UserProfilePage/UserBookings/UserBookings.js
+++ frontend/src/components/UserProfilePage/UserBookings/UserBookings.js
@@ -55,35 +55,39 @@ function UserBookings() {
                         <li className="booking-list-item" key={booking.id}>
                             <div className="single-booking-container">
                                 <div className="bookings-image-container">
-                                    <img src={booking?.listing?.Images[0]?.imageUrl} />
+                                    <img className="bookings-image" src={booking?.listing?.Images[0]?.imageUrl} />
                                 </div>
                                 <div className="align-container">
                                     {/* <div className="bookings-title">{booking?.listing?.title}</div> */}
-                                <div className="new-data-container">
-                                    <div className="bookings-data-container">
-                                        <div >
-                                            <li><span>Booked on: </span>{booking?.createdAt && new Date(booking.createdAt).toDateString()}</li>
-                                            <li><span>Start date: </span>{booking?.startDate && new Date(booking.startDate).toDateString()}</li>
-                                            <li><span>End date: </span>{booking?.endDate && new Date(booking.endDate).toDateString()}</li>
-                                            <li><span>Hosted by: </span>{booking?.listing?.User?.username}</li>
-                                        </div>
-                                        <div>
-                                            <li><span>Bathrooms: </span>{booking?.listing?.bathrooms}</li>
-                                            <li><span>Bedrooms: </span>{booking?.listing?.bedrooms}</li>
-                                            <li><span>Price: </span>${booking?.listing?.price} / night</li>
+                                    <div className="new-data-container">
+                                        <div className="bookings-data-container">
+                                            <div >
+                                                <ul>
+                                                    <li><span>Booked on: </span>{booking?.createdAt && new Date(booking.createdAt).toDateString()}</li>
+                                                    <li><span>Start date: </span>{booking?.startDate && new Date(booking.startDate).toDateString()}</li>
+                                                    <li><span>End date: </span>{booking?.endDate && new Date(booking.endDate).toDateString()}</li>
+                                                    <li><span>Hosted by: </span>{booking?.listing?.User?.username}</li>
+                                                </ul>
+                                            </div>
+                                            <div>
+                                                <ul>
+                                                    <li><span>Bathrooms: </span>{booking?.listing?.bathrooms}</li>
+                                                    <li><span>Bedrooms: </span>{booking?.listing?.bedrooms}</li>
+                                                    <li><span>Price: </span>${booking?.listing?.price} / night</li>
+                                                </ul>
+                                            </div>
+                                            <div>
+                                                <ul>
+                                                    <li><span>Address: </span>{booking?.listing?.address}</li>
+                                                    <li><span>City: </span>{booking?.listing?.city}</li>
+                                                    <li><span>State: </span>{booking?.listing?.state}</li>
+                                                    <li><span>Zip code: </span>{booking?.listing?.zipCode}</li>
+                                                    <li><span>Country: </span>{booking?.listing?.country}</li>
+                                                </ul>
+                                            </div>
 
                                         </div>
-                                        <div>
-                                            <li><span>Address: </span>{booking?.listing?.address}</li>
-                                            <li><span>City: </span>{booking?.listing?.city}</li>
-                                            <li><span>State: </span>{booking?.listing?.state}</li>
-                                            <li><span>Zip code: </span>{booking?.listing?.zipCode}</li>
-                                            <li><span>Country: </span>{booking?.listing?.country}</li>
-
-                                        </div>
-
                                     </div>
-                                </div>
                                 </div>
                                 <button onClick={handleCancelBooking} id={`cancel-${booking?.id}`} className="cancel-booking-button remove-listing-button">Cancel booking</button>
                             </div>
